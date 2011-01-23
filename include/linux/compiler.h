c5a9a5412641268233e2b45a837c302347f0cb5a
Christian Lamparter <chunkeey@googlemail.com>
2011-01-23 22:01:05 +0100

    carl9170 firmware: fix gcc alignment warnings
    warning: cast increases required alignment of target type.
    
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -78,7 +78,7 @@
 
 #define container_of(ptr, type, member) ({			\
 	const typeof(((type *)0)->member) * __mptr = (ptr);	\
-	(type *)((char *)__mptr - offsetof(type, member)); })
+	(type *)(((unsigned long)__mptr - offsetof(type, member))); })
 
 #define MAX_ERRNO	4095
 
