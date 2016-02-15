d33a9b7ba0c5428531610c589c2cdaf5e1f1123c
Christian Lamparter <chunkeey@googlemail.com>
2016-02-15 01:15:02 +0100

    carl9170 firmware: reserve feature flag for PATTERN_GENERATOR
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>

--- include/shared/fwdesc.h
+++ include/shared/fwdesc.h
@@ -84,6 +84,9 @@ enum carl9170fw_feature_list {
 	/* Firmware has support to write a byte at a time  */
 	CARL9170FW_HAS_WREGB_CMD,
 
+	/* Pattern generator */
+	CARL9170FW_PATTERN_GENERATOR,
+
 	/* KEEP LAST */
 	__CARL9170FW_FEATURE_NUM
 };
