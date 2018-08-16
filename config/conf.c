a9ddd9e866faef305376bf826b868b36bebf59ed
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-08-16 16:48:26 +0900

    kconfig: suppress "configuration written to .config" for syncconfig
    The top-level Makefile invokes "make syncconfig" when necessary.
    Then, Kconfig displays the following message when .config is updated.
    
      #
      # configuration written to .config
      #
    
    It is distracting because "make syncconfig" happens during the build
    stage, and does nothing important in most cases.
    
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- config/conf.c
+++ config/conf.c
@@ -494,6 +494,14 @@ int main(int ac, char **av)
 		}
 		input_mode = (enum input_mode)opt;
 		switch (opt) {
+		case syncconfig:
+			/*
+			 * syncconfig is invoked during the build stage.
+			 * Suppress distracting "configuration written to ..."
+			 */
+			conf_set_message_callback(NULL);
+                        sync_kconfig = 1;
+			break
 		case defconfig:
 		case savedefconfig:
 			defconfig_file = optarg;
