a57a7e78baeede9c999a67dcd812fbfc711463fa
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-05-11 01:56:48 +0900

    kconfig: remove useless NULL pointer check in conf_write_dep()
    conf_write_dep() has just one caller:
    
        conf_write_dep("include/config/auto.conf.cmd");
    
    "name" always points to a valid string.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- config/confdata.c
+++ config/confdata.c
@@ -1010,8 +1010,6 @@ static int conf_write_dep(const char *name)
 	struct file *file;
 	FILE *out;
 
-	if (!name)
-		name = ".kconfig.d";
 	out = fopen("..config.tmp", "w");
 	if (!out)
 		return 1;
