397a9f814016e48e54c704039a0d8d779367bc59
Masahiro Yamada <masahiroy@kernel.org>
2024-02-03 00:58:04 +0900

    kconfig: write Kconfig files to autoconf.cmd in order
    Currently, include/config/autoconf.cmd saves included Kconfig files in
    reverse order. While this is not a big deal, it is inconsistent with
    other *.cmd files generated by fixdep.
    
    Output the included Kconfig files in the included order.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- config/confdata.c
+++ config/confdata.c
@@ -20,6 +20,8 @@
 
 #include "lkc.h"
 
+struct gstr autoconf_cmd;
+
 /* return true if 'path' exists, false otherwise */
 static bool is_present(const char *path)
 {
@@ -1021,7 +1023,6 @@ end_check:
 static int conf_write_autoconf_cmd(const char *autoconf_name)
 {
 	char name[PATH_MAX], tmp[PATH_MAX];
-	struct file *file;
 	FILE *out;
 	int ret;
 
@@ -1042,9 +1043,7 @@ static int conf_write_autoconf_cmd(const char *autoconf_name)
 		return -1;
 	}
 
-	fprintf(out, "deps_config := \\\n");
-	for (file = file_list; file; file = file->next)
-		fprintf(out, "\t%s \\\n", file->name);
+	fputs(str_get(&autoconf_cmd), out);
 
 	fprintf(out, "\n%s: $(deps_config)\n\n", autoconf_name);
 
