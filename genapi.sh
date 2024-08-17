594b1dcfd376e7bcee9e027ce6b8323cb79b4ca6
Christian Lamparter <chunkeey@gmail.com>
2024-08-17 21:51:06 +0200

    build: add double quotes for git version
    not having those leads to the following error:
    
    | version.h:6:32: error: too many decimal points in number
    |    6 | #define CARL9170FW_VERSION_GIT 1.9.9-481-g7854d00
    |                                       ^~~~~
    | ...
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- genapi.sh
+++ genapi.sh
@@ -6,6 +6,6 @@ cat <<EOF > include/shared/version.h
 #define CARL9170FW_VERSION_YEAR $((100$(date +%Y)%100))
 #define CARL9170FW_VERSION_MONTH $((100$(date +%m)%100))
 #define CARL9170FW_VERSION_DAY $((100$(date +%d)%100))
-#define CARL9170FW_VERSION_GIT $(git describe 2>/dev/null)
+#define CARL9170FW_VERSION_GIT "$(git describe 2>/dev/null)"
 #endif /* __CARL9170_SHARED_VERSION_H */
 EOF
