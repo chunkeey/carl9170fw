3f5103c30bb4dd8f537d7f0f3b0d358c82b91a14
MeiChia Chiu <MeiChia.Chiu@mediatek.com>
2024-11-12 16:38:46 +0800

    wifi: mac80211: Support EHT 1024 aggregation size in TX
    Support EHT 1024 aggregation size in TX
    
    The 1024 agg size for RX is supported but not for TX.
    This patch adds this support and refactors common parsing logics for
    addbaext in both process_addba_resp and process_addba_req into a
    function.
    
    Reviewed-by: Shayne Chen <shayne.chen@mediatek.com>
    Reviewed-by: Money Wang <money.wang@mediatek.com>
    Co-developed-by: Peter Chiu <chui-hao.chiu@mediatek.com>
    Signed-off-by: Peter Chiu <chui-hao.chiu@mediatek.com>
    Signed-off-by: MeiChia Chiu <MeiChia.Chiu@mediatek.com>
    Link: https://patch.msgid.link/20241112083846.32063-1-MeiChia.Chiu@mediatek.com
    [pass elems/len instead of mgmt/len/is_req]
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- include/linux/ieee80211.h
+++ include/linux/ieee80211.h
@@ -1454,6 +1454,8 @@ struct ieee80211_mgmt {
 					__le16 status;
 					__le16 capab;
 					__le16 timeout;
+					/* followed by BA Extension */
+					u8 variable[];
 				} __packed addba_resp;
 				struct{
 					u8 action_code;
